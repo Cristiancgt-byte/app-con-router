db5db8d18281bdbdfc99f1b78479b076a42be1a8
Cristiancgt-byte <cristian.gambat@uniagustiniana.edu.co>
2025-10-15 16:19:28 +0000

    segundo commit

---
+++ mi-app-con-router/src/componentes/PokemonDetail.js
@@ -0,0 +1,51 @@
+import react, { useState, useEffect } from "react";
+
+function Tableta() {
+    const [pokemons, setPokemons] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+
+    useEffect(()=> {
+        const fetchPokemons = async () => {
+            try{
+                setLoading(true);
+                const response = await fetch(
+                    "https://pokeapi.co/api/v2/pokemon?limit=100"
+                );
+
+                if(!responsive.ok) {
+                    throw new Error("Error al cargar los pokemon");
+                }
+
+                const data = await response.json();
+                setPokemons(data.results);
+                setLoading(false);
+             } catch (err) {
+                setError(err.message);
+                setLoading(false);
+             }
+        };
+
+        fetchPokemons();
+    
+    },[]); // Ell array vacio significa que se ejecuta solo una vez al montar
+
+    if(loading) return <div>Cargando porfavor espere...</div>;
+
+    if(error) return <div>Error: {error}</div>;
+
+    return (
+        <div>
+            <h1>Lista de pokemon</h1>
+            <ul>
+                {pokemons.map((pokemon, index) => ( 
+                    <li key={index}>{pokemon.name}</li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default Tableta;
+
